8c21eb87607b5f75ff6adb2ad1c53b31313313a5
LilSem <evgenyi_sit@mail.ru>
2022-11-15 21:15:52 +0300

    feat: поправил ошибки для прохождения автотестов

--- scripts/data.js
+++ scripts/data.js
@@ -47,10 +47,10 @@ const imagePreview = document.querySelector('.figure__preview');
 const descriptionPreview = document.querySelector('.figure__description');
 
 // Popup names
-const popupProfile = document.querySelector('.popup__profile');
-const popupAddCard = document.querySelector('.popup__add-card');
-const popupPreview = document.querySelector('.popup__preview');
+const popupProfile = document.querySelector('.popup_profile');
+const popupAddCard = document.querySelector('.popup_card');
+const popupPreview = document.querySelector('.popup_preview');
 
 // Form
-const profilePopupForm = document.querySelector('.form__profile');
-const addCardPopupForm = document.querySelector('.form__add-card');
+const profilePopupForm = document.querySelector('.form_profile');
+const addCardPopupForm = document.querySelector('.form_card');
